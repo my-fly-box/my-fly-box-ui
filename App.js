0952a991783e5c132db05a533faec9cd1548090e
Rachael Thomas <rnthomas001@gmail.com>
2020-07-18 11:51:50 -0600

    Create starting navigation for the app

--- App.js
+++ App.js
@@ -1,13 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import FlyBox from './containers/FlyBox';
+import AddFly from './containers/AddFly';
+import EditFly from './containers/EditFly';
+
+const Tab = createMaterialBottomTabNavigator();
+const FlyBoxStack = createStackNavigator();
+const AddFlyStack = createStackNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator initialRouteName="MyFlyBox">
+      <Tab.Screen name="MyFlyBox" component={FlyBoxContainer} />
+      <Tab.Screen name="AddFly" component={AddFlyContainer} />
+    </Tab.Navigator>
+  );
+}
+
+function FlyBoxContainer() {
+  return (
+    <FlyBoxStack.Navigator>
+      <FlyBoxStack.Screen name="MyFlyBox" component={FlyBox} />
+      <FlyBoxStack.Screen name="EditFly" component={EditFly} /> 
+    </FlyBoxStack.Navigator>
+  )
+}
+
+function AddFlyContainer() {
+  return (
+    <AddFlyStack.Navigator>
+      <AddFlyStack.Screen name="AddFly" component={AddFly} />
+    </AddFlyStack.Navigator>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
   );
 }
 
