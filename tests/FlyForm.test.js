cc6f0f950f3b8ce5189d1215a8cce5074d1bdeca
Charlie Bandstra <bandstracharlie@gmail.com>
2020-07-20 13:58:05 -0600

    remove test guts

--- tests/FlyForm.test.js
+++ tests/FlyForm.test.js
@@ -6,8 +6,6 @@ import renderer from 'react-test-renderer'
 
 describe('FlyForm', () => {
   it('renders the FlyForm', () => {
-    const flyNameText = 'Name'
-    const flyForm = renderer.create(<FlyForm/>)
-    const textInst = flyForm.root.findAllByType(Text)
+    
   })
 })
