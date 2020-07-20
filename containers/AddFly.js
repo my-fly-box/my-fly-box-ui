7e68b21692edf63646038ba81863beae47116e91
Tim Nguyen <tim.nguyen2112@gmail.com>
2020-07-20 11:38:03 -0600

    Clean up white spaces in code

--- containers/AddFly.js
+++ containers/AddFly.js
@@ -6,17 +6,17 @@ import { addFly, clearFlyEntry } from '../actions';
 import { addFlyToAPI } from "../ApiCalls";
 
 class AddFly extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
   addFly = () => {
     const currentFlyEntry = this.props.currentFlyEntry
     addFlyToAPI(currentFlyEntry)
       .then(data => {
-          this.props.addFly(data.data);
-          this.props.clearFlyFields();
-        }
+        this.props.addFly(data.data);
+        this.props.clearFlyFields();
+      }
     )
   }
 
@@ -27,18 +27,18 @@ class AddFly extends Component {
         
         <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
           <TouchableOpacity style={styles.button}
-              onPress = {() => {
-                this.props.clearFlyFields();
-                this.props.navigation.navigate('MyFlyBox');
-                }}>
+            onPress = {() => {
+              this.props.clearFlyFields();
+              this.props.navigation.navigate('MyFlyBox');
+              }}>
             <Text style={styles.button}>Cancel</Text>
           </TouchableOpacity>
         
           <TouchableOpacity style={styles.button}
-              onPress = {() => {
-                this.addFly()
-                this.props.navigation.navigate('MyFlyBox');
-                }}>
+            onPress = {() => {
+              this.addFly()
+              this.props.navigation.navigate('MyFlyBox');
+              }}>
             <Text style={styles.button}>Submit</Text>
           </TouchableOpacity>
         </View>
@@ -48,27 +48,27 @@ class AddFly extends Component {
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        paddingTop: 45,
-    },
-    button: {
-        backgroundColor: '#00a8d5',
-        color: 'white',
-        margin: 5,
-        height: 35,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
+  container: {
+    flex: 1,
+    paddingTop: 45,
+  },
+  button: {
+    backgroundColor: '#00a8d5',
+    color: 'white',
+    margin: 5,
+    height: 35,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 })
 
 const mapStateToProps = state => ({
-    currentFlyEntry: state.currentFlyEntry,
-  })
+  currentFlyEntry: state.currentFlyEntry,
+})
 
 const mapDispatchToProps = (dispatch) => ({
-    addFly: data => dispatch( addFly(data) ),
-    clearFlyFields: () => dispatch( clearFlyEntry() ),
-  })
+  addFly: data => dispatch( addFly(data) ),
+  clearFlyFields: () => dispatch( clearFlyEntry() ),
+})
 
 export default connect(mapStateToProps, mapDispatchToProps)(AddFly);
--- reducers/index.js
+++ reducers/index.js
@@ -2,7 +2,6 @@ import { combineReducers } from 'redux';
 import { currentFlies } from './currentFlies';
 import { currentFlyEntry } from './currentFlyEntry'
 
-
 const rootReducer = combineReducers({
 	currentFlies,
 	currentFlyEntry,
