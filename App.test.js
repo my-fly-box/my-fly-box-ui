899b3d47afaf97dc4157e60a46d8b85825a4da84
Charlie Bandstra <bandstracharlie@gmail.com>
2020-07-20 14:01:31 -0600

    Add app test blueprint

---
+++ App.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text } from 'react-native'
+import App from './App'
+
+import renderer from 'react-test-renderer'
+
+describe('App', () => {
+  it('renders the App', () => {
+    
+  })
+})
