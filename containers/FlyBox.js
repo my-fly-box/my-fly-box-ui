7261e7f5cad61887381977b41a09029ca76f790e
Rachael Thomas <rnthomas001@gmail.com>
2020-07-20 10:24:24 -0600

    Add API call to componentDidMount and remove the test data

--- containers/FlyBox.js
+++ containers/FlyBox.js
@@ -1,133 +1,100 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
-import Fly from '../components/Fly';
-import { setFlies } from '../actions'
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Button, FlatList } from "react-native";
+import Fly from "../components/Fly";
+import { setFlies } from "../actions";
 import { connect } from "react-redux";
-
-// Rachael: this is mocked out data that will be removed once the API requests are up and running
-const TestFlies = [
-    {
-        id: 1,
-        name: "Parachute Adams",
-        size: 18,
-        color: "tan",
-        category: "dry",
-        amount: 2,
-        isFavorite: false,
-        user_id: 1,
-        created_at: "07/18/2020",
-        updated_at: null
-    },
-    {
-        id: 2,
-        name: "Copper John",
-        size: 20,
-        color: "copper",
-        category: "midge",
-        amount: 1,
-        isFavorite: false,
-        user_id: 1,
-        created_at: "07/19/2020",
-        updated_at: null
-    },
-    {
-        id: 1,
-        name: "Zebra Midge",
-        size: 22,
-        color: "red",
-        category: "midge",
-        amount: 3,
-        isFavorite: false,
-        user_id: 1,
-        created_at: "07/17/2020",
-        updated_at: null
-    }
-]
+import { fetchFlies } from "../ApiCalls";
 
 const FlatListItemSeparator = () => {
-    return (
-        <View style={{ height: 1.5, width: "100%", backgroundColor: "#7A5C58"}} />
-    )
-}
+  return (
+    <View style={{ height: 1.5, width: "100%", backgroundColor: "#7A5C58" }} />
+  );
+};
 
 class FlyBox extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    async componentDidMount() {
-        this.props.setFlies(TestFlies)
-    }
+  async componentDidMount() {
+    fetchFlies().then((data) => this.props.setFlies(data.data));
+  }
 
-    checkFlyBox = () => {
-        if (this.props.currentFlies.length > 0) {
-            return (
-                <FlatList
-                    style={styles.fly}
-                    ItemSeparatorComponent={FlatListItemSeparator} 
-                    data={this.props.currentFlies}
-                    renderItem={({item}) => 
-                        <Fly fly={item} navigation={this.props.navigation} />
-                    }
-                />
-            )
-        }
-        else {
-            return (
-                <View style={{ alignItems: "center", justifyContent: "center", paddingTop: 50}}>
-                    <Text style={{ fontSize: 30, fontWeight: "bold"}}>Your fly box is empty.</Text>
-                </View>
-            )
-        }
+  checkFlyBox = () => {
+    if (this.props.currentFlies.length > 0) {
+      return (
+        <FlatList
+          style={styles.fly}
+          ItemSeparatorComponent={FlatListItemSeparator}
+          data={this.props.currentFlies}
+          renderItem={({ item }) => (
+            <Fly fly={item} navigation={this.props.navigation} />
+          )}
+        />
+      );
+    } else {
+      return (
+        <View
+          style={{
+            alignItems: "center",
+            justifyContent: "center",
+            paddingTop: 50,
+          }}
+        >
+          <Text style={{ fontSize: 30, fontWeight: "bold" }}>
+            Your fly box is empty.
+          </Text>
+        </View>
+      );
     }
+  };
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <View style={styles.header}>
-                    <Text style={styles.headerFont}>Fly Name</Text>
-                    <Text style={styles.headerFont}>Fly Type</Text>
-                    <Text style={styles.headerFont}>Size</Text>
-                    <Text style={styles.headerFont}>Edit</Text>
-                </View>
-                {this.checkFlyBox()}
-            </View>
-        )
-    }
-    
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.headerFont}>Fly Name</Text>
+          <Text style={styles.headerFont}>Fly Type</Text>
+          <Text style={styles.headerFont}>Size</Text>
+          <Text style={styles.headerFont}>Edit</Text>
+        </View>
+        {this.checkFlyBox()}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#E8E9F3'
-    },
-    fly: {
-        height: 44,
-        width: "100%",
-      },
-    header: {
-        flexDirection: "row",
-        justifyContent: "space-around",
-        width: "100%",
-        paddingVertical: 10,
-        borderBottomColor: "#2A9D8F",
-        borderBottomWidth: 2,
-        backgroundColor: "#BDC667"
-    },
-    headerFont: {
-        fontSize: 18,
-        fontWeight: "bold",
-        color: "#2A9D8F"
-    }
-  });
+  container: {
+    flex: 1,
+    backgroundColor: "#E8E9F3",
+  },
+  fly: {
+    height: 44,
+    width: "100%",
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    width: "100%",
+    paddingVertical: 10,
+    borderBottomColor: "#2A9D8F",
+    borderBottomWidth: 2,
+    backgroundColor: "#BDC667",
+  },
+  headerFont: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#2A9D8F",
+  },
+});
 
-  const mapStateToProps = state => ({
-    currentFlies: state.currentFlies,
-  })
+const mapStateToProps = (state) => ({
+  currentFlies: state.currentFlies,
+});
 
-  const mapDispatchToProps = (dispatch) => ({
-    setFlies: data => dispatch( setFlies(data) ),
-  })
+const mapDispatchToProps = (dispatch) => ({
+  setFlies: (data) => dispatch(setFlies(data)),
+});
 
-  export default connect(mapStateToProps, mapDispatchToProps)(FlyBox);
+export default connect(mapStateToProps, mapDispatchToProps)(FlyBox);
