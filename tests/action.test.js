8393d1b1b5cac2214183740539eaa264a40ac1aa
Tim Nguyen <tim.nguyen2112@gmail.com>
2020-07-20 18:01:44 -0600

    Add additional tets for App FlyForm and Actions components

--- tests/action.test.js
+++ tests/action.test.js
@@ -33,4 +33,56 @@ describe('action creators', () => {
 		expect(result1).toEqual(expectedAction1);
 		expect(result2).toEqual(expectedAction2);
 	})
+
+	it('should have a type of ADD_FLY', () => {
+		const newFlyData = [
+			{
+				"name": "Tom",
+				"size": 4,
+				"color": "red",
+				"category": "fliessss",
+				"amount": 1
+			}
+		]
+
+		const expectedAction = {
+			type: 'ADD_FLY',
+			data: newFlyData
+		}
+
+		const result = actions.addFly(newFlyData)
+
+		expect(result).toEqual(expectedAction);
+	})
+
+	it('should have a type of UPDATE_FLY_ENTRY', () => {
+		const expectedAction1 = {
+			type: 'UPDATE_FLY_ENTRY',
+			field: "name",
+			data: "Bobby"
+		}
+
+		const expectedAction2 = {
+			type: 'UPDATE_FLY_ENTRY',
+			field: "size",
+			data: 5
+		}
+
+		const result1 = actions.updateFlyEntry('name', 'Bobby')
+		const result2 = actions.updateFlyEntry('size', 5)
+
+		expect(result1).toEqual(expectedAction1);
+		expect(result2).toEqual(expectedAction2);
+	})
+
+	it('should have a type of CLEAR_FLY_ENTRY', () => {
+		const expectedAction = {
+			type: 'CLEAR_FLY_ENTRY',
+		}
+
+		const result = actions.clearFlyEntry()
+
+		expect(result).toEqual(expectedAction);
+	})
+
 })
