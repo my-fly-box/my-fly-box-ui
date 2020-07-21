bf37433096d89b91ded625aaeaab5fb586dd4ea8
Tim Nguyen <tim.nguyen2112@gmail.com>
2020-07-20 18:31:04 -0600

    Add testing for the currentFlies rducer

--- tests/Redux/currentFliesReducer.test.js
+++ tests/Redux/currentFliesReducer.test.js
@@ -1,16 +1,10 @@
 import { currentFlies } from '../../reducers/currentFlies'
 
 describe('currentFlies Reducer', () => {
-	it("should return the intial state", () => {
-		const expectedResult = []
-		
-    const result = currentFlies(undefined, {});
+	let fliesData, newFlyData;
 
-    expect(result).toEqual(expectedResult)
-	})
-	
-	it("when receiving SET_FLIES action, should return a new array of Fly objects", () => {
-		const fliesData = [
+	beforeEach(() => {
+		fliesData = [
 			{
 				"id": "8",
 				"type": "fly",
@@ -37,6 +31,30 @@ describe('currentFlies Reducer', () => {
 			}
 		]
 
+		newFlyData = 
+		{
+			"id": "9",
+			"type": "fly",
+			"attributes": {
+				"id": 9,
+				"name": "New Tom",
+				"size": 6,
+				"color": "orange",
+				"category": "dirt fly",
+				"amount": 2
+				}
+		}
+	})
+
+	it("should return the intial state", () => {
+		const expectedResult = []
+		
+    const result = currentFlies(undefined, {});
+
+    expect(result).toEqual(expectedResult)
+	})
+	
+	it("when receiving SET_FLIES action, should return a new array of Fly objects", () => {
 		const action = {type: 'SET_FLIES', data: fliesData}
 
 		const expectedResult = fliesData
@@ -44,5 +62,55 @@ describe('currentFlies Reducer', () => {
 		const result = currentFlies(undefined, action)
 
     expect(result).toEqual(expectedResult)
+	})
+	
+	it("when receiving ADD_FLY action, should return an udpated array of Fly objects", () => {
+		const updatedFliesData = [
+			{
+				"id": "8",
+				"type": "fly",
+				"attributes": {
+					"id": 8,
+					"name": "Tom",
+					"size": 4,
+					"color": "red",
+					"category": "fliessss",
+					"amount": 1
+					}
+			},
+			{
+				"id": "11",
+				"type": "FLY",
+				"attributes": {
+					"id": 11,
+					"name": "JERRY",
+					"size": 8,
+					"color": "Blue",
+					"category": "dry fly",
+					"amount": 3
+					}
+			},
+			{
+				"id": "9",
+				"type": "fly",
+				"attributes": {
+					"id": 9,
+					"name": "New Tom",
+					"size": 6,
+					"color": "orange",
+					"category": "dirt fly",
+					"amount": 2
+					}
+			}
+		]
+
+		const action = {type: 'ADD_FLY', data: newFlyData}
+
+		const expectedResult = updatedFliesData
+
+		const result = currentFlies(fliesData, action)
+
+    expect(result).toEqual(expectedResult)
   })
+
 })
