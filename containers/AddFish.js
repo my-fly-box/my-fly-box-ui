99c21420995218dd3b7a6051a9d06cee5198d418
Tim Nguyen <tim.nguyen2112@gmail.com>
2020-07-22 15:14:48 -0600

    Udpate Add Fish Form to have buttons to either cancel or submit fish entry

--- containers/AddFish.js
+++ containers/AddFish.js
@@ -1,10 +1,60 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, Button, FlatList } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { connect } from "react-redux";
 
-export default function AddFish({ navigation }) {
-  return (
-    <View>
-      <Text>Add Fish</Text>
-    </View>
-  );
-};
+class AddFish extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        
+        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
+          <TouchableOpacity style={styles.button}
+            onPress = {() => {
+              this.props.navigation.navigate('MyFlyBox');
+              alert('go to home page')
+              }}>
+            <Text style={styles.button}>Cancel</Text>
+          </TouchableOpacity>
+        
+          <TouchableOpacity style={styles.button}
+            onPress = {() => {
+              this.props.navigation.navigate('MyFlyBox');
+              alert('submit fish')
+              }}>
+            <Text style={styles.button}>Submit</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 45,
+  },
+  button: {
+    backgroundColor: '#00a8d5',
+    color: 'white',
+    margin: 5,
+    height: 35,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
+
+// const mapStateToProps = state => ({
+//   currentFlyEntry: state.currentFlyEntry,
+// })
+
+// const mapDispatchToProps = (dispatch) => ({
+//   addFly: data => dispatch( addFly(data) ),
+//   clearFlyFields: () => dispatch( clearFlyEntry() ),
+// })
+
+export default connect(null, null)(AddFish);
