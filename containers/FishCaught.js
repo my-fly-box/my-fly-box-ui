71aba4e61d379eedf5d575e00891bb0cfab0b5cb
Rachael Thomas <rnthomas001@gmail.com>
2020-07-22 13:52:16 -0600

    Change FishCaught to a class component, add in Redux for GSM, and add a componentDidMount to fetch the fish data when the page loads

--- containers/FishCaught.js
+++ containers/FishCaught.js
@@ -7,20 +7,34 @@ import {
   FlatList,
   TouchableOpacity,
 } from "react-native";
+import { fetchFish} from "../ApiCalls";
+import { setFish } from "../actions";
+import { connect } from "react-redux";
 
-export default function FishCaught ({ navigation }) {
-  return (
-    <View>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => {
-          navigation.navigate("AddFish");
-        }}
-      >
-        <Text style={styles.buttonText}>Add Fish</Text>
-      </TouchableOpacity>
-    </View>
-  );
+class FishCaught extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  async componentDidMount () {
+    fetchFish().then((data) => this.props.setFish(data.data));
+  }
+
+  render() {
+    return (
+      <View>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            navigation.navigate("AddFish");
+          }}
+        >
+          <Text style={styles.buttonText}>Add Fish</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
 };
 
 const styles = StyleSheet.create({
@@ -39,3 +53,14 @@ const styles = StyleSheet.create({
     textAlign: "center",
   },
 });
+
+const mapStateToProps = (state) => ({
+  currentFish: state.currentFish,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  setFish: (data) => dispatch(setFish(data)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(FishCaught);
+
