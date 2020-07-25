6b9d5ba6c7dbc4bc303d413c38aeef018d5c709d
Rachael Thomas <rnthomas001@gmail.com>
2020-07-25 10:59:09 -0600

    Adjust styling of upload image button to align with the merged styling changes Charlie made

--- containers/AddFish.js
+++ containers/AddFish.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { connect } from "react-redux";
-import FishForm from '../components/FishForm'
-import { clearFishEntry, addFish } from '../actions';
+import FishForm from "../components/FishForm";
+import { clearFishEntry, addFish } from "../actions";
 import { addFishToAPI } from "../ApiCalls";
 
 class AddFish extends Component {
@@ -11,48 +11,55 @@ class AddFish extends Component {
   }
 
   addFish = () => {
-    const currentFishEntry = this.props.currentFishEntry
-    addFishToAPI(currentFishEntry)
-      .then(data => {
-        this.props.addFish(data.data);
-        this.props.clearFishEntry();
-      }
-    )
-  }
+    const currentFishEntry = this.props.currentFishEntry;
+    addFishToAPI(currentFishEntry).then((data) => {
+      this.props.addFish(data.data);
+      this.props.clearFishEntry();
+    });
+  };
 
   render() {
     return (
       <View style={styles.container}>
         {/* working on getting camera integrated */}
-        <TouchableOpacity style={styles.button}
-            onPress = {() => {
-              this.props.navigation.navigate('PhotoSelector');
-              }}>
-            <Text style={styles.button}>Upload Image</Text>
+
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              this.props.navigation.navigate("PhotoSelector");
+            }}
+          >
+            <Text>Upload Image</Text>
           </TouchableOpacity>
+        </View>
 
         <FishForm />
-        
+
         <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button}
-            onPress = {() => {
-              this.props.navigation.navigate('FishCaught');
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              this.props.navigation.navigate("FishCaught");
               this.props.clearFishEntry();
-              }}>
+            }}
+          >
             <Text>Cancel</Text>
           </TouchableOpacity>
-        
-          <TouchableOpacity style={styles.button}
-            onPress = {() => {
+
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
               this.addFish();
-              this.props.navigation.navigate('FishCaught');
+              this.props.navigation.navigate("FishCaught");
               this.props.clearFishEntry();
-              }}>
+            }}
+          >
             <Text>Submit</Text>
           </TouchableOpacity>
         </View>
       </View>
-    )
+    );
   }
 }
 
@@ -61,44 +68,44 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 45,
     width: "100%",
-    height:"100%",
-    justifyContent: 'center',
+    height: "100%",
+    justifyContent: "center",
   },
   buttonContainer: {
     width: "80%",
-    height:"100%", 
-    flex: 1, 
-    flexDirection: 'row',
+    height: "100%",
+    flex: 1,
+    flexDirection: "row",
     alignSelf: "center",
   },
   button: {
-    backgroundColor: 'white',
-    color: 'white',
+    backgroundColor: "white",
+    color: "white",
     flex: 1,
     alignSelf: "center",
     margin: 5,
     marginTop: "20%",
     height: "20%",
     width: "40%",
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     borderWidth: 2,
-    borderColor: '#2A9D8F',
+    borderColor: "#2A9D8F",
     borderRadius: 3,
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.8,
     shadowRadius: 2,
   },
-})
+});
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   currentFishEntry: state.currentFishEntry,
-})
+});
 
 const mapDispatchToProps = (dispatch) => ({
-  clearFishEntry: () => dispatch( clearFishEntry() ),
-  addFish: data => dispatch( addFish(data) ),
-})
+  clearFishEntry: () => dispatch(clearFishEntry()),
+  addFish: (data) => dispatch(addFish(data)),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(AddFish);
